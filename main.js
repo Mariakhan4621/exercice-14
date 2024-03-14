1d288413bafc97e7e927d52cb7b898df233fbbbc
Mariakhan4621 <160171512+Mariakhan4621@users.noreply.github.com>
2024-03-14 19:51:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var guestlist = ["salman", "maria", "saiqa", "iqra"];
+guestlist.forEach(function (oneGuest) { return console.log("Salam ".concat(oneGuest, " Would you like to dinner with me?")); });
---
+++ main.ts
@@ -0,0 +1,4 @@
+let guestlist = ["salman","maria","saiqa","iqra"];
+
+guestlist.forEach(oneGuest => console.log(`Salam ${oneGuest} Would you like to dinner with me?`));
+
